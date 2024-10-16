cba219e9ac256e8cdc4b8de1d4faa7b881a177f7
l4v3nx <ygiki52@gmail.com>
2024-10-17 01:47:03 +0300

    back to blueberry

--- .config/ags/user_options.js
+++ .config/ags/user_options.js
@@ -4,61 +4,73 @@
 //   options listed in this file will override the default ones in the above file
 
 const userConfigOptions = {
-    'ai': {
-        'defaultGPTProvider': "oxygen4o",
-        'proxyUrl': "http://127.0.0.1:8080",
+    ai: {
+        defaultGPTProvider: "oxygen4o",
+        proxyUrl: "http://127.0.0.1:8080",
     },
-    'appearance': {
-        'fakeScreenRounding': 0,
+    appearance: {
+        fakeScreenRounding: 0,
     },
-    'apps': {
-        'bluetooth': "blueman-manager",
-        'imageViewer': "eog",
-        'settings': "XDG_CURRENT_DESKTOP=\"gnome\" gnome-control-center",
-        'taskManager': "htop",
-        'terminal': "foot", // This is only for shell actions
+    apps: {
+        imageViewer: "eog",
+        settings: 'XDG_CURRENT_DESKTOP="gnome" gnome-control-center',
+        taskManager: "htop",
+        terminal: "foot", // This is only for shell actions
     },
-    'cheatsheet': {
-        'keybinds': {
-            'configPath': "~/.config/hypr/custom/keybinds.conf"
-        }
-    },
-    'dock': {
-        'enabled': true,
-        'pinnedApps': ['org.gnome.Settings', 'org.gnome.Software', 'org.gnome.tweaks', 'org.gnome.Nautilus', 'firefox', 'foot', 'code-url-handler', 'steam', 'org.telegram.desktop', 'spotify-launcher', 'com.obsproject.Studio', 'vesktop'],
-        'monitorExclusivity': true, // Dock will move to other monitor along with focus if enabled
-        'trigger': [], // client_added, client_move, workspace_active, client_active
-    },
-    'sidebar': {
-        'image': {
-            'allowNsfw': true,
-        },
-        'pages': {
-            'order': ["apis", "tools"],
-            'apis': {
-                'order': ["gpt", "waifu", "booru", "gemini"],
-            }
+    cheatsheet: {
+        keybinds: {
+            configPath: "~/.config/hypr/custom/keybinds.conf",
         },
     },
-    'search': {
-        'enableFeatures': {
-            'directorySearch': false,
-            'aiSearch': false,
-        },
+    dock: {
+        enabled: true,
+        pinnedApps: [
+            "org.gnome.Settings",
+            "org.gnome.Software",
+            "org.gnome.tweaks",
+            "org.gnome.Nautilus",
+            "firefox",
+            "foot",
+            "code-url-handler",
+            "steam",
+            "org.telegram.desktop",
+            "spotify-launcher",
+            "com.obsproject.Studio",
+            "vesktop",
+        ],
+        monitorExclusivity: true, // Dock will move to other monitor along with focus if enabled
+        trigger: [], // client_added, client_move, workspace_active, client_active
     },
-    'keybinds': {
-        'cheatsheet': {
-            'keybinds': {
-                'nextTab': "End",
-                'prevTab': "Home",
+    sidebar: {
+        image: {
+            allowNsfw: true,
+        },
+        pages: {
+            order: ["apis", "tools"],
+            apis: {
+                order: ["gpt", "waifu", "booru", "gemini"],
             },
-            'nextTab': "Ctrl+End",
-            'prevTab': "Ctrl+Home",
-        }
+        },
     },
-    'bar': {
-        'utilities': ['snip', 'picker', 'keyboard', 'camera'],
-    }
-}
+    search: {
+        enableFeatures: {
+            directorySearch: false,
+            aiSearch: false,
+        },
+    },
+    keybinds: {
+        cheatsheet: {
+            keybinds: {
+                nextTab: "End",
+                prevTab: "Home",
+            },
+            nextTab: "Ctrl+End",
+            prevTab: "Ctrl+Home",
+        },
+    },
+    bar: {
+        utilities: ["snip", "picker", "keyboard", "camera"],
+    },
+};
 
 export default userConfigOptions;
