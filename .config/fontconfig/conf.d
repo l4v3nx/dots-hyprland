af7090673e48d71369d63518777edab79b8453b7
l4v3nx <ygiki52@gmail.com>
2024-10-23 15:15:07 +0300

    add qt6ct config

--- .config/fontconfig/conf.d
+++ .config/fontconfig/conf.d
@@ -1,9 +1,25 @@
-<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+<?xml version='1.0'?>
+<!DOCTYPE fontconfig SYSTEM 'urn:fontconfig:fonts.dtd'>
 <fontconfig>
-    <match target="font">
-        <edit name="rgba" mode="assign">
-        <const>none</const>
-    </edit>
-  </match>
+ <match target="font">
+  <edit mode="assign" name="rgba">
+   <const>rgb</const>
+  </edit>
+ </match>
+ <match target="font">
+  <edit mode="assign" name="hinting">
+   <bool>true</bool>
+  </edit>
+ </match>
+ <match target="font">
+  <edit mode="assign" name="hintstyle">
+   <const>hintfull</const>
+  </edit>
+ </match>
+ <match target="font">
+  <edit mode="assign" name="antialias">
+   <bool>true</bool>
+  </edit>
+ </match>
+ <dir>~/.local/share/fonts</dir>
 </fontconfig>
