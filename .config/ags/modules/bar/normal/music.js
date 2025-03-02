1d1465ef0b314b120303beeaa39432ab240e63c5
l4v3nx <ygiki52@gmail.com>
2025-03-02 16:55:03 +0300

    music: replace triggering player controls with play-pause on lmb

--- .config/ags/modules/bar/normal/music.js
+++ .config/ags/modules/bar/normal/music.js
@@ -27,7 +27,7 @@ function trimTrackTitle(title) {
 }
 
 function adjustVolume(direction) {
-    const step = 0.03; 
+    const step = 0.03;
     execAsync(['playerctl', 'volume'])
         .then((output) => {
             let currentVolume = parseFloat(output.trim());
@@ -234,9 +234,9 @@ export default () => {
                 // SystemResourcesOrCustomModule(),
                 EventBox({
                     child: BarGroup({ child: musicStuff }),
-                    onPrimaryClick: () => showMusicControls.setValue(!showMusicControls.value),
+                    onPrimaryClick: () => execAsync('playerctl play-pause').catch(print),
                     onSecondaryClick: () => execAsync(['bash', '-c', 'playerctl next || playerctl position `bc <<< "100 * $(playerctl metadata mpris:length) / 1000000 / 100"` &']).catch(print),
-                    onMiddleClick: () => execAsync('playerctl play-pause').catch(print),
+                    onMiddleClick: () => execAsync('playerctl previous').catch(print),
                     setup: (self) => self.on('button-press-event', (self, event) => {
                         if (event.get_button()[1] === 8) // Side button
                             execAsync('playerctl previous').catch(print)
